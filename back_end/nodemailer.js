4e7cc37cd48216466a6d408a534bf561e91ef5b4
Ilona Tykhonovskaya <ilonatiho@gmail.com>
2026-09-16 12:48:05 +0200

    1

--- back_end/nodemailer.js
+++ back_end/nodemailer.js
@@ -1,9 +1,19 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+// const transporter = nodemailer.createTransport({
+//     host: process.env.MAIL_HOST,
+//     port: Number(process.env.MAIL_PORT),
+//     secure: process.env.MAIL_SECURE === "true",
+//     auth: {
+//         user: process.env.MAIL_USER,
+//         pass: process.env.MAIL_PASS
+//     }
+// });
 const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: Number(process.env.MAIL_PORT),
-    secure: process.env.MAIL_SECURE === "true",
+    host: mailHost,
+    port: isGmailSmtp ? 465 : Number(process.env.MAIL_PORT),
+    secure: isGmailSmtp ? true : process.env.MAIL_SECURE === "true",
+    family: 4,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
